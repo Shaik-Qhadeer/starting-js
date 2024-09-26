8247bba1eaa6e639f296f602a713e2759e7dd104
SHAIK QHADEER <115864883+Uniqueprogrammer@users.noreply.github.com>
2024-09-26 12:25:39 +0000

    nums and math

---
+++ 01_Basics/nums_and_maths.js
@@ -0,0 +1,32 @@
+const num = 120
+const score = 400
+// console.log(score);
+ 
+ const balance = new Number(400)
+//  console.log(balance.toString().length);
+ 
+//  console.log(balance.toFixed(2));
+
+ const currency = 123.345
+//  console.log(currency.toPrecision(3));
+
+ const hundreds = 1000000
+//  console.log(hundreds.toLocaleString('en-IN'));
+
+//maths
+
+
+ console.log(Math.random());
+ console.log((Math.random()*10)+1);
+
+ const min=10
+ const max=20
+ console.log(Math.floor(Math.random() * (max-min + 1)) + min);
+ 
+ 
+
+ 
+ 
+ 
+ 
+ 
