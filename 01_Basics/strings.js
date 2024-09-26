12e9af0d45c2037bb5edf8cee4a89b830692f402
SHAIK QHADEER <115864883+Uniqueprogrammer@users.noreply.github.com>
2024-09-26 07:08:29 +0000

    strings-completed

---
+++ 01_Basics/strings.js
@@ -0,0 +1,30 @@
+const name = "shaik qhadeer "
+const age = 21
+let studies= "btech"
+
+// console.log(name +  age);
+  
+// console.log(`my name is ${name} and my age is ${age} and i am currently in my ${studies}`);
+
+const gameName = new String('  qhadeer sk      ')
+
+// console.log(gameName[1]);
+
+
+// console.log(gameName.indexOf('r'));
+// console.log(gameName.toUpperCase());
+
+// const newGame = gameName.slice(0,4)
+// console.log(newGame);
+// console.log(gameName.split(' '));
+console.log(gameName.trimEnd());
+
+const url = "https://shaikqhadeer.com/yaba%20shaik"
+console.log(url.replace('%20','-'));
+console.log(url.includes('baba'));
+
+
+
+
+
+
