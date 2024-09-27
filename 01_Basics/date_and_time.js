57e1c54d2a46bb073914da368caf946e3c778915
SHAIK QHADEER <115864883+Uniqueprogrammer@users.noreply.github.com>
2024-09-27 01:18:07 +0000

    data and time

---
+++ 01_Basics/date_and_time.js
@@ -0,0 +1,11 @@
+let myDate = new Date()
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toISOString());
+
+let myCreatedDate = new Date("2024-01-23")
+// console.log(myCreatedDate.toLocaleDateString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+
